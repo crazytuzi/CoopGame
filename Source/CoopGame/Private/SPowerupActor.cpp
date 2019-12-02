78fe5637570d87c0b7f62ebaade76cea1602987e
crazytuzi <957824770@qq.com>
2019-12-02 21:37:45 +0800

    Create the Power-up Class

---
+++ Source/CoopGame/Private/SPowerupActor.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPowerupActor.h"
+
+// Sets default values
+ASPowerupActor::ASPowerupActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	PowerupInterval = 0.f;
+	TotalNrOfTicks = 0;
+}
+
+// Called when the game starts or when spawned
+void ASPowerupActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASPowerupActor::OnTickPowerup()
+{
+	++TicksProcessed;
+
+	OnPowerupTicked();
+
+	if (TotalNrOfTicks >= TicksProcessed)
+	{
+		OnExpired();
+
+		// Delete timer
+		GetWorldTimerManager().ClearTimer(TimeHandle_PowerupTick);
+	}
+}
+
+void ASPowerupActor::ActivatePowerup()
+{
+	if (PowerupInterval > 0.f)
+	{
+		GetWorldTimerManager().SetTimer(TimeHandle_PowerupTick, this, &ASPowerupActor::OnTickPowerup, PowerupInterval,
+		                                true, 0.f);
+	}
+	else
+	{
+		OnTickPowerup();
+	}
+}
