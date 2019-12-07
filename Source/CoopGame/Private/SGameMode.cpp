a202843859862a2d602851e0ea91ebd8674b6a66
crazytuzi <957824770@qq.com>
2019-12-07 19:38:22 +0800

    Add Wave Based Spawning

--- Source/CoopGame/Private/SGameMode.cpp
+++ Source/CoopGame/Private/SGameMode.cpp
@@ -2,4 +2,51 @@
 
 
 #include "SGameMode.h"
+#include "TimerManager.h"
 
+
+ASGameMode::ASGameMode()
+{
+	TimeBetweenWaves = 2.f;
+}
+
+void ASGameMode::StartWave()
+{
+	++WaveCount;
+
+	NrOfBotsToSpawn = 2 * WaveCount;
+
+	GetWorldTimerManager().SetTimer(TimerHandle_BotSpawner, this, &ASGameMode::SpawnBotTimerElapsed, 1.f, true, 0.f);
+}
+
+void ASGameMode::EndWave()
+{
+	GetWorldTimerManager().ClearTimer(TimerHandle_BotSpawner);
+
+	PrepareForNextWave();
+}
+
+void ASGameMode::PrepareForNextWave()
+{
+	FTimerHandle TimerHandle_NextWaveStart;
+	GetWorldTimerManager().SetTimer(TimerHandle_NextWaveStart, this, &ASGameMode::StartWave, 1.f, false);
+}
+
+void ASGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	PrepareForNextWave();
+}
+
+void ASGameMode::SpawnBotTimerElapsed()
+{
+	SpawnNewBot();
+
+	--NrOfBotsToSpawn;
+
+	if (NrOfBotsToSpawn <= 0)
+	{
+		EndWave();
+	}
+}
